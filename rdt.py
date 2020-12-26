0181e1291ffb33f986676e0998364b43fb22cfbe
MrPenguins <56572306+MrPenguins@users.noreply.github.com>
2020-12-26 23:54:16 +0800

    add tips for states

--- rdt.py
+++ rdt.py
@@ -69,12 +69,15 @@ class RDTSocket(UnreliableSocket):
             if data_client.syn == 1:  # 收到连接请求
                 self.recvSin = True
                 self.ackNum = data_client.seqNumber + 1
+                print("receive connection request!")
             conn.sendto(segment(sin=1, ack=1, ackNumber=self.ackNum).getSegment(), addr_client)  # 发sin ack
+            print("send ack")
             while True:
                 data_client2, addr_client2 = self.recvfrom(1024)
                 data_client2 = segment.parse(data_client2)
                 if data_client2.syn == 1 and data_client2.ack == 1 and addr_client2 == addr_client and data_client2.seqNumber == self.ackNum:  # 收到了原来的地址发来的正确报文
                     conn.connectAddr = addr_client  # 建立连接
+                    print("connection established")
                     # conn.sendto(segment(ack=1, ackNumber=data_client2.seqNumber + 1).getSegment(), addr_client)
                     self.reset()  # 重置socket
                     break
@@ -96,13 +99,16 @@ class RDTSocket(UnreliableSocket):
         #############################################################################
         self.connectAddr = address
         self.sendto(segment(sin=1).getSegment(), self.connectAddr)  # 发送请求连接报文
+        print("send connect request")
         data_sever, addr_sever = self.recvfrom(1024)
         data_sever = segment.parse(data_sever)
         if data_sever.ack == 1 and data_sever.sin == 1 and addr_sever == self.connectAddr:
+            print("received ack")
             self.seqNum = 1
             self.ackNum = data_sever.seqNumber + 1
             self.sendto(segment(sin=1, ack=1, seqNumber=self.seqNum, ackNumber=self.ackNum).getSegment(),
                         self.connectAddr)
+            print("send ack")
 
         #############################################################################
         #                             END OF YOUR CODE                              #
